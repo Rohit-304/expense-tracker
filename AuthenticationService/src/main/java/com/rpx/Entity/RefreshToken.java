97c52f0509869ef2ae09bf1e4623a52f6eab29d6
Rohit Sahu <113296241+Rohit-304@users.noreply.github.com>
2025-11-11 10:58:02 +0530

    @commit(auth service & gateway service created).

---
+++ AuthenticationService/src/main/java/com/rpx/Entity/RefreshToken.java
@@ -0,0 +1,5 @@
+package com.rpx.Entity;
+
+public class RefreshToken {
+
+}
---
+++ AuthenticationService/src/main/java/com/rpx/Entity/UserRole.java
@@ -0,0 +1,47 @@
+package com.rpx.Entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "user_role")
+public class UserRole {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(name = "name")
+	private String name;
+
+	public UserRole() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public UserRole(Long id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
