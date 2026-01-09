8a808a5b94ce121b43613e0e52b3af286edf055f
Rohit Sahu <113296241+Rohit-304@users.noreply.github.com>
2026-01-09 14:33:50 +0530

    @Commit(Notification Service, Api-Gateay, Expense Tracker Created)

--- NotificationService/src/main/java/com/rpx/notification/NotificationServiceApplication.java
+++ NotificationService/src/main/java/com/rpx/notification/NotificationServiceApplication.java
@@ -1,13 +1,13 @@
-package com.rpx.AuthenticationService;
+package com.rpx.notification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AuthenticationServiceApplication {
+public class NotificationServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AuthenticationServiceApplication.class, args);
+		SpringApplication.run(NotificationServiceApplication.class, args);
 	}
 
 }
